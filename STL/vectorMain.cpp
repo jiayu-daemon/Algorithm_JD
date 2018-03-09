f4124b3ca8b2b57acbd5cec04f304a8b8bdd0d92
jiayu <jiayu_daemon@163.com>
2018-03-09 17:39:29 +0800

    add vector

---
+++ STL/vectorMain.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <typename T> void printVector(const vector <T> & intergers2);
+
+
+int main()
+{
+
+	const int SIZE = 6;
+	int array[SIZE] = {1,2,3,4,5,6};
+
+	vector <int > intergers;
+	cout << "The initial size of intergers is :" << intergers.size()
+	<< "\nThe initial capacity of integers is " << intergers.capacity();
+
+
+	intergers.push_back(2);
+	intergers.push_back(3);
+	intergers.push_back(4);
+
+	cout << "\nThe size of integers is:" << intergers.size()
+	<< "\nThe capacity of integers is:" << intergers.capacity();
+	cout << "\n\nOutput array using pointer notation";
+
+	
+}
